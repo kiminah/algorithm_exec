5addeec5ab275f260bb9326498abb5ab8f723a45
inah <kimih1200@gmail.com>
2021-05-17 21:37:24 +0900

    Modify programmers-비밀지도(런타임에러 해결)

--- programmers_level_01/programmers_비밀지도.java
+++ programmers_level_01/programmers_비밀지도.java
@@ -3,14 +3,29 @@ class Solution {
         String[] answer = new String[n];
         
         for(int i=0; i<n; i++){
-            // 주어진 숫자를 이진수로 변환
-            // OR를 통해 arr1과 arr2을 합쳐줌
-            // 이때 자릿수를 맞춰줘야함
-            answer[i] = String.format("%0"+n+"d", Integer.parseInt(Integer.toBinaryString(arr1[i] | arr2[i])));
+            // arr1 과 arr2를 OR로 결합해서 이진수로 변환
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
             
-            // 지도를 만들기 위해 부호화된 문자들을 암호화 시킴
-            answer[i] = answer[i].replaceAll("1","#");
-            answer[i] = answer[i].replaceAll("0"," ");
+            // 주어진 값이 n보다 짧으면 0을 앞부분에 추가
+            if(answer[i].length() < n){
+                String tmp = "";
+                for(int j=0; j<n-answer[i].length(); j++){
+                    tmp += "0";
+                }
+                answer[i] = tmp + answer[i];
+            }
+            
+            // 부호화된 숫자를 암호화 시키기
+            String tmp = "";
+            for(int j=0; j<n; j++){
+                
+                if(answer[i].charAt(j) == '1'){
+                    tmp += "#";
+                }else{
+                    tmp += " ";
+                }
+            }
+            answer[i] = tmp;
         }
         
         return answer;
