099540b146962d3e0af412662fe24052482a9909
inah <kimih1200@gmail.com>
2021-06-24 16:22:44 +0900

    Modify backjoon-피보나치3

--- backjoon/backjoon_피보나치수3.java
+++ backjoon/backjoon_피보나치수3.java
@@ -8,19 +8,20 @@ public class backjoon_피보나치수3 {
      * 즉, K가 1,000,000이면 피사노 주기는 1,500,000
      */
     static long[] memo;
+    static int pisano;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);     
         long N = sc.nextLong();
-        memo = new long[10000];
-        // 0과 겹치지 않기 위해 -1로 채우기
-        Arrays.fill(memo, -1);
-        System.out.println(fibo((int)N));
-    }
+        pisano = 1500000; // 피사노 주기
+        memo = new long[pisano]; 
+        memo[0] = 0;
+        memo[1] = 1;
+        for(int i=2; i<pisano; i++)
+            memo[i] = (memo[i-1]+memo[i-2]) % 1000000;
+        
+        if(N >= pisano) N %= pisano;
 
-    public static long fibo(int n){
-        if(n<=1) return n;
-        if(memo[n] == -1)
-            memo[n] = fibo(n-1) + fibo(n-2);
-        return memo[n];
+        // N이 long 형태이기 때문에 int형으로 변경해줘야 함
+        System.out.println(memo[(int)N]);
     }
 }
