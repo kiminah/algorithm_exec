4e93fa0f03952d87d156296fd0dcc51237a83ec2
inah <kimih1200@gmail.com>
2021-06-29 14:31:17 +0900

    Modify programmers-영어 끝말잇기(완료)

--- programmers_level_02/programmers_영어끝말잇기.java
+++ programmers_level_02/programmers_영어끝말잇기.java
@@ -1,49 +1,40 @@
 import java.util.*;
 class Solution {
-    boolean[] visit;
-    Queue<String> q;
     ArrayList<String> al;
-    int[] answer = new int[2];
-    
     public int[] solution(int n, String[] words) {
+        int[] answer = new int[n];
         
-        visit = new boolean[words.length];
-        q = new LinkedList<>();
+        // 1. 지금까지 언급된 단어들 저장 
+        //    첫 번째 단어 미리 저장
         al = new ArrayList<>();
+        al.add(words[0]);
         
-        solve(n,words);
-        
-        return find_fail(n);
-    }
-    
-    public void solve(int n, String[] words){
-        q.add(words[0]);
-        
-        while(!q.isEmpty()){
-            String w = q.poll();
-            boolean flag = false;
+        // 2. 두 번째 단어부터 끝말잇기 검토
+        int stage = 1;
+        for(int i=1; i<words.length; i++){
             
-            for(int i=0; i<words.length; i++){
-                if(w.charAt(w.length()-1)==words[i].charAt(0)){
-                    flag = true;
-                    q.add(words[i]);
-                    al.add(words[i]);
-                }
+            // 2-1. n으로 나눈 나머지로 몇 번째 사람인지 파악
+            //      i가 0부터 시작하기 때문에 i+1
+            //      순서가 첫 번째 사람으로 돌아오면 stage 증가
+            //      0인 경우, n으로 나눠진 경우이기 때문에 n번째 사람으로 인식
+            int person = (i+1)%n; 
+            if(person == 1) stage++;
+            else if(person == 0) person = n;
+            
+            // 2-2. 지금까지 언급된 단어가 있는지 확인
+            //      언급된 적 있으면 리턴
+            //      언급된 적 없으면 배열에 추가
+            if(al.contains(words[i])) return new int[] {person, stage};
+            else al.add(words[i]);
+            
+            // 2-3. 단어와 끝과 처음의 알파벳 비교
+            //      다르면 리턴
+            if(words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)){
+                return new int[] {person,stage};
             }
         }
-    }
-    public int[] find_fail(int n){
-        int stage=1;
-        for(int i=1; i<al.size(); i++){
-            if(i%3==0) stage++;
-            if(al.get(i-1).charAt(al.get(i-1).length()-1) != al.get(i).charAt(0)){
-                int person = i%n;
-                if(person==0) person = n;
-                return new int[] {person, stage};
-            }
-        }
-        return new int[] {0,0};
         
+        // 3. 탈라자 발생하지 않은 경우
+        return new int[] {0,0};
     }
-    
 }
