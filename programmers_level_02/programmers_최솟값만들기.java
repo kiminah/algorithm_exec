819beb9bf2b7f353ed5718bf1f0a65154454350d
inah <kimih1200@gmail.com>
2021-07-09 14:20:41 +0900

    Modify programmers-최솟값 만들기(dfs -> arraylist)

--- programmers_level_02/programmers_최솟값만들기.java
+++ programmers_level_02/programmers_최솟값만들기.java
@@ -1,38 +1,27 @@
-
+import java.util.*;
 class Solution
 {
-    boolean[] visit;
-    int answer = Integer.MAX_VALUE;
-    
     public int solution(int[] A, int[] B)
     {
-        int len = A.length;
+        int answer = 0;
+
+        ArrayList<Integer> a_q = new ArrayList<>();
+        ArrayList<Integer> b_q = new ArrayList<>();
+
+        // 1. A와 B를 정렬 (A의 최소값과 B의 최대값을 곱한 값 = 두 수 곱의 합 최소값)
+        for(int i=0; i<A.length; i++){
+            a_q.add(A[i]);
+            b_q.add(B[i]);
+        }
+        Collections.sort(a_q);
+        Collections.sort(b_q, Collections.reverseOrder());
         
-        visit = new boolean[len*2];
-        solve(A, B, len, 0, 0);
+        // 2. 두 수의 곱을 합산
+        for(int i=0; i<a_q.size(); i++){
+            answer += a_q.get(i) * b_q.get(i);
+        }
         
         return answer;
     }
     
-    public void solve(int[] A, int[] B, int len, int sum, int depth){
-        
-        if(depth==len && answer > sum){
-            answer = sum;             
-            return;
-        }
-        
-        for(int i=0; i<len; i++){
-            if(!visit[i]){
-                visit[i] = true;
-                for(int j=0; j<len; j++){
-                    if(!visit[j+len]){
-                        visit[j+len] = true;
-                        solve(A, B, len, sum+(A[i]*B[j]), depth+1);
-                        visit[j+len] = false;
-                    }
-                }
-                visit[i] = false;
-            }
-        }
-    }
 }
