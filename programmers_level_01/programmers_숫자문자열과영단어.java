4c465830fa6064f5ac49de309d7fc69c81cfa6ab
inah <kimih1200@gmail.com>
2021-07-21 21:23:16 +0900

    Modify programmers-숫자 문자열과 영단어(처음에 푼 방법 추가)

--- programmers_level_01/programmers_숫자문자열과영단어.java
+++ programmers_level_01/programmers_숫자문자열과영단어.java
@@ -1,5 +1,11 @@
 class Solution {
     public int solution(String s) {
+        /** << 처음에 푼 방법 >> 
+         * Map을 이용하여 영단어 map을 생성
+         * 문자열을 비교하여 숫자면 answer에 합치고
+         * 아니라면 임시 문자열에 추가하여 map에 매칭되는 key가 있으면
+         * 숫자로 변경 후 임시 문자열 초기화
+         */
         
         // 1. 영단어 배열 만들기
         String[] num = {"zero", "one", "two", "three", "four", "five",
