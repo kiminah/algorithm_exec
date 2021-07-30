d29ef09ae9c90d933fb8c5e64f0d10dc5f515f9a
inah <kimih1200@gmail.com>
2021-07-30 12:20:50 +0900

    Modify programmers-N개의 최소공배수(주석 추가)

--- programmers_level_02/programmers_N개의최소공배수.java
+++ programmers_level_02/programmers_N개의최소공배수.java
@@ -3,6 +3,7 @@ class Solution {
     public int solution(int[] arr) {
         int answer = arr[0];
         for(int i=1; i<arr.length; i++){
+            // 두 수의 최소공배수
             answer = min_num(answer, arr[i]);
         }
         return answer;
@@ -12,6 +13,7 @@ class Solution {
     public int min_num(int n1, int n2){
         int result = 1;
         for(int i=2; i<=Math.max(n1,n2);i++){
+            // 최대 공약수
             if(n1%i==0 & n2%i==0){
                 result *= i;
                 n1 /= i;
@@ -19,6 +21,7 @@ class Solution {
                 i=1;
             }
         }
+        // 최대공약수로 나눈 나머지 수 곱
         result *= n1*n2;
         return result;
     }
