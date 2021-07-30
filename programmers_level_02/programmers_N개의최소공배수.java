b89d82fba67b8251cae8378b74c22861c8de2043
inah <kimih1200@gmail.com>
2021-07-30 12:23:27 +0900

    Modify programmers-N개의 최소공배수(효율성)

--- programmers_level_02/programmers_N개의최소공배수.java
+++ programmers_level_02/programmers_N개의최소공배수.java
@@ -2,27 +2,21 @@ import java.util.*;
 class Solution {
     public int solution(int[] arr) {
         int answer = arr[0];
+        int g;
         for(int i=1; i<arr.length; i++){
-            // 두 수의 최소공배수
-            answer = min_num(answer, arr[i]);
+            // 1. 최대공약수
+            g = gcb(answer, arr[i]);
+            // 2. 앞서 구한 최대공약수를 이용한 최대공배수
+            answer = g * (answer/g) * (arr[i]/g);
         }
         return answer;
         
     }
     
-    public int min_num(int n1, int n2){
-        int result = 1;
-        for(int i=2; i<=Math.max(n1,n2);i++){
-            // 최대 공약수
-            if(n1%i==0 & n2%i==0){
-                result *= i;
-                n1 /= i;
-                n2 /= i;
-                i=1;
-            }
-        }
-        // 최대공약수로 나눈 나머지 수 곱
-        result *= n1*n2;
-        return result;
+    public int gcb(int n1, int n2){
+        if(n1>n2)
+            return (n1%n2==0) ? n2:gcb(n2, n1%n2);
+        else
+            return (n2%n1==0) ? n1:gcb(n1, n2%n1);
     }
 }
