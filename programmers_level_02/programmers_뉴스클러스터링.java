9171e8d2adb86265c72812925d51df9f61b5d788
inah <kimih1200@gmail.com>
2021-08-09 15:15:06 +0900

    Modify programmers-뉴스 클러스터링(기존 코드와 완전 다른 코드)

--- programmers_level_02/programmers_뉴스클러스터링.java
+++ programmers_level_02/programmers_뉴스클러스터링.java
@@ -1,64 +1,64 @@
 import java.util.*;
 class Solution {
-    int total_cnt = 0;
+    ArrayList<String> multiSet1;
+    ArrayList<String> multiSet2;
+    ArrayList<String> union;
+    ArrayList<String> intersection;
     public int solution(String str1, String str2) {
-        int answer = 0;
-        // 대소문자 차이 무시
+        
+        multiSet1 = new ArrayList<>();
+        multiSet2 = new ArrayList<>();
+        union = new ArrayList<>();
+        intersection = new ArrayList<>();
+        
+        // 1. 대소문자 차이 무시
         str1 = str1.toUpperCase();
         str2 = str2.toUpperCase();
         
-        // 같은 문자인 경우
-        if(str1.equals(str2))
-            return 65536;
+        // 2. 다중집합 생성
+        multiSet(str1, 1);
+        multiSet(str2, 2);
         
-        // 1. 각 문자별 원소생성
-        String[] arr1 = union(str1);
-        String[] arr2 = union(str2);
-
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        // 2. 교집합 
-        Set<String> set = new HashSet<>();
-        for(int i=0; i<arr1.length; i++){
-            String tmp = arr1[i];
-            if(set.add(tmp)){
-                
-                int a = 0;
-                int b = 0;
-                
-                for(String arr:arr1)
-                    if(arr.equals(tmp)) a++;
-                
-                for(String arr:arr2)
-                    if(arr.equals(tmp)) b++;
-                
-                total_cnt += Math.min(a,b);
+        // 3. 정렬
+        Collections.sort(multiSet1);
+        Collections.sort(multiSet2);
+        
+        // 4. 교집합과 합집합 생성
+        for(String str : multiSet1){
+            // 4-1. 동일한 문자이면 교집합에 추가
+            //      다중집합 B에서 삭제
+            if(multiSet2.remove(str)){
+                intersection.add(str);
             }
+            // 4-2. 합집합에는 모든 문자 추가
+            union.add(str);
+        }
+        // 5. 교집합이 제거된 다중집합 B의 나머지 요소 합집합에 추가
+        for(String str : multiSet2){
+            union.add(str);
         }
         
-        answer = (arr1.length + arr2.length) - total_cnt;
-        answer = (int)((double)total_cnt/answer * 65536);
+        double answer = 0;
+        if(union.size() == 0) 
+            answer = 1;
+        else 
+            answer = (double)intersection.size()/(double)union.size();
         
-        return answer;
+        return  (int)(answer * 65536);
     }
-    public String[] union(String str){
-        String result = "";
-        String tmp = "";
-        for(int i=0; i<str.length(); i++){
-            char c = str.charAt(i);
-            if(c >= 'A' && c<='Z'){
-                tmp += c;
-            }else{
-                tmp = "";
-            }
-
-            if(tmp.length()==2){
-                result += tmp + ",";
-                tmp = c+"";
+    
+    public void multiSet(String str, int num){
+        for(int i=0; i<str.length()-1; i++){
+            char first = str.charAt(i);
+            char second = str.charAt(i+1);
+            
+            if(first >= 'A' && first <= 'Z' &&
+               second >= 'A' && second <= 'Z'){
+                if(num==1)
+                    multiSet1.add(first+""+second);
+                else
+                    multiSet2.add(first+""+second);
             }
         }
-        result = result.substring(0,result.length()-1);
-        return result.split(",");
-        
     }
 }
