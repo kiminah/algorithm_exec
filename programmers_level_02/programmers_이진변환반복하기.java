aa3e06eb92e9851b6777f4daf7f95945e1b82797
inah <kimih1200@gmail.com>
2021-08-11 15:18:16 +0900

    Modify programmers-이진 변환 반복하기(효율적인 코드로 수정)

--- programmers_level_02/programmers_이진변환반복하기.java
+++ programmers_level_02/programmers_이진변환반복하기.java
@@ -1,25 +1,24 @@
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
+        int[] answer = new int[2];
         
-        int loop = 0;
-        int cnt = 0;
         // 1이 될때까지 반복
         while(!s.equals("1")){
-            String tmp = "";
-            // 1. 0제거
-            for(int i=0; i<s.length(); i++){
-                if(s.charAt(i) == '1'){
-                    tmp += s.charAt(i);
-                }else 
-                    cnt++; // 0의 개수
-            }
-            // 2. 0 제거한 문자열 길이를 이진법으로 변환
-            s = Integer.toBinaryString(tmp.length());
-            // 3. 이진변환 횟수
-            loop++;
+            // 1. 문자열 길이 저장
+            answer[1] += s.length();
+            // 2. 0 제거
+            s = s.replaceAll("0", "");
+            int tmp = s.length();
+            // 3. 0 제거한 문자열 길이를 이진법 변환
+            s = Integer.toBinaryString(tmp);
+
+            // 이진변환 횟수 증가
+            answer[0]++;
+            // 제거된 0의 개수 = 전체 문자열 - 1의 개수
+            answer[1] -= tmp;
         }
         
-        return new int[] {loop, cnt};
+        return answer;
     }
 }
