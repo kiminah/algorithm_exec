7ee341ecea33c66dd4e272995ed308ab1c72aa70
inah <kimih1200@gmail.com>
2021-08-19 21:31:08 +0900

    Modify programmers-2개 이하로 다른 비트

--- programmers_level_02/programmers_2개이하로다른비트.java
+++ programmers_level_02/programmers_2개이하로다른비트.java
@@ -2,19 +2,27 @@ class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
         
-        int idx = 0; // answer 넣을 index
-
+        int idx = 0;
         for(long num : numbers){
-            long next = num; // 다음 수
-            int cnt = Integer.MAX_VALUE; // 다른 비트의 개수
-            // 다른 비트의 개수가 2개 이하이면 종료
-            while(cnt > 2){
-                next++;
-                cnt = Long.toBinaryString(num^next).replaceAll("0", "").length();
-            }
-            answer[idx++] = next;
+            // 이진수로 변환
+            String num_str = Long.toBinaryString(num);
+            // 연속된 1의 개수
+            long cnt = count(num_str);
+            // 1의 최소 1개를 갖고 있어야 한다.(짝수의 경우 이진수 끝이 0 이므로)
+            if(cnt==0) cnt = 1;
+            // 1의 개수만큼 10진법으로 더해주기
+            else cnt = (long)Math.pow(2,cnt-1);
+            
+            answer[idx++] = num+cnt;
         }
-        
         return answer;
+    }
+    public long count(String str){
+        long cnt = 0;
+        for(int i=str.length()-1; i>=0;i--){
+            if(str.charAt(i)=='0') break;
+            cnt++;
+        }
+        return cnt;
     }
 }
