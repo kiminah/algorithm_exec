d55a04e092226c1509bb00d3b4d008b12d1296b8
inah <kimih1200@gmail.com>
2021-09-03 16:10:17 +0900

    Modify programmers-이상한 문자 만들기(cnt 코드 변경)

--- programmers_level_02/programmers_이상한문자만들기.java
+++ programmers_level_02/programmers_이상한문자만들기.java
@@ -2,17 +2,16 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         
-        String[] word = s.split(""); // 공백도 포함하기 위해서
+        String[] wordList = s.split(""); // 공백도 포함하기 위해서
         int cnt = 0; // s의 idx
-        for(int i=0; i<word.length; i++){
-            // 공백인 경우 idx 초기화
-            if(word[i].equals(" ")){
-                cnt = -1;
-            }
-            // 짝수번째 문자를 대문자, 홀수번째 문자를 소문자로 변경
-            answer += (cnt%2 == 0)? word[i].toUpperCase() : word[i].toLowerCase();
-            cnt++; // idx 증가
+        for(String word : wordList){
+            // 공백인 경우 초기화, 아닌경우 증가
+            cnt = word.equals(" ") ? 0 : cnt+1;
+            // cnt를 1부터 시작하기 때문에 
+            // 홀수인 경우를 대문자로, 짝수인 경우를 소문자로 변경
+            answer += (cnt%2 != 0) ? word.toUpperCase() : word.toLowerCase();
         }
+
         return answer;
     }
 }
