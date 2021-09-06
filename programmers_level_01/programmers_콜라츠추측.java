50bf3f98f3ec82e349fb796ed75d4815e121112b
inah <kimih1200@gmail.com>
2021-09-06 14:22:07 +0900

    Modify programmers-콜라츠 추측(간결한 코드 추가)

--- programmers_level_01/programmers_콜라츠추측.java
+++ programmers_level_01/programmers_콜라츠추측.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int num) {
         int answer = 0;
-        long n = num; // 형변환
+        long n = (long)num; // 형변환
         // answer 500번 되면 -1 반환
         while(answer < 500){
             if(n == 1) break; // n이 1이 되면 반복문 종료
@@ -10,7 +10,18 @@ class Solution {
             n = (n%2==0)? n/2 : (n*3)+1;
             answer++;
         }
-        
         return (answer==500) ? -1 : answer;
+
+        /** 더 간결한 코드
+        long n = (long)num;
+        // 500번 반복
+        for(int i=0; i<500; i++){
+            if(n == 1) return i; // n이 1이 되는 순간 반복 횟수 리턴
+            // n의 값 짝,홀수에 따라 변경
+            n = (n%2==0)? n/2 : (n*3)+1;
+        }
+        // 반복문 500번 시도한 경우 -1 리턴
+        return -1;
+         */
     }
 }
