a58658b80727fcbeed686f4acc605438486441c2
inah <kimih1200@gmail.com>
2021-09-09 13:53:40 +0900

    Modify programmers-문자열 내 마음대로 정렬하기(다른 방법 추가)

--- programmers_level_01/programmers_문자열내마음대로정렬하기.java
+++ programmers_level_01/programmers_문자열내마음대로정렬하기.java
@@ -12,5 +12,27 @@ class Solution {
         });
         
         return answer;
+
+        /** 다른 방법 (실행시간은 길다)
+         * 
+            // 1. n번째 글자를 index값으로 맨 앞에 추가
+            List<String> list = new ArrayList<>()
+            for(String str : strings){
+                list.add(str.charAt(n)+str);
+            }
+
+            // 2. 정렬
+            Collections.sort(list);
+
+            // 3. index 값으로 설정한 맨 앞글자를 제거한 문자를 추가
+            String[] answer = new String[list.size()];
+            for(int i=0; i<list.size(); i++){
+                answer[i] = list.get(i).substring(1);
+            }
+            
+            return answer;
+
+        */
+
     }
 }
